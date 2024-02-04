806379249ef4b4475da83fb07a333b9caf47ff57
RajaATAli <allmdararslan13@gmail.com>
2024-02-04 12:06:02 -0500

    Script changed to select more than five different products

--- Lab2_MATLAB_Loops_And_Vectors/Ali_lab2_.m
+++ Lab2_MATLAB_Loops_And_Vectors/Ali_lab2_.m
@@ -17,16 +17,16 @@
 % * Find the total income generated from all sales
 
 
-% Preparing the data and displaying it to the user
+% Preparing the data and displaying it to the user (more than 5 products)
 % Sources for shortlisting products (best selling PCs in 2017)
 % * https://www.foxbusiness.com/markets/best-selling-pcs-of-2017
 % * https://finance.yahoo.com/news/best-selling-pcs-2017-230300126.html
 % The price and quantity for each product is hypothetical but looks real,
 % fulfilling the requirements for the assignment
 % Data is initialized using vectors
-products = {'HP 8300 Elite Small Form Factor Desktop Computer', 'HP 22-b016 All-in-One desktop', 'HP Pavilion Power 580-023w Gaming Tower', 'Aspire E 15-inch notebook', 'HP 14 Inch "Stream" Laptop'};
-price = [500, 450, 750, 600, 220];
-quantity_2017 = [150000, 120000, 100000, 180000, 200000];
+products = {'HP 8300 Elite Small Form Factor Desktop Computer', 'HP 22-b016 All-in-One desktop', 'HP Pavilion Power 580-023w Gaming Tower', 'Aspire E 15-inch notebook', 'HP 14 Inch "Stream" Laptop',' Dell OptiPlex, Intel Core 2 Duo'};
+price = [500, 450, 750, 600, 220, 480];
+quantity_2017 = [150000, 120000, 100000, 180000, 200000, 110000];
 
 % Displaying the data using a for loop
 % Also computing the average price of all products within the loop to 
@@ -34,15 +34,15 @@ quantity_2017 = [150000, 120000, 100000, 180000, 200000];
 disp("Initialized Product Data (2017):");
 sumproducts = 0;
 for i = 1:length(products)
-    fprintf('Product = %s; Price = %.2f; Quantity = %d\n',products{i}, price(i), quantity_2017(i));
+    fprintf('Product = %s; Price ($) = %.2f; Quantity = %d\n',products{i}, price(i), quantity_2017(i));
     sumproducts = sumproducts + price(i);
 end
 
 avgPriceAllProducts = sumproducts/length(products);
-fprintf('\nThis is the average price of all products = %.3f\n', avgPriceAllProducts)
+fprintf('\nThis is the average price of all products ($) = %.3f\n', avgPriceAllProducts)
 
 % Debug step to check if average calculated correctly
-% disp(mean(price))
+%disp(mean(price))
 
 
 % Finding how many products were sold at a price above the average price (use
@@ -58,7 +58,7 @@ for i = 1:length(price)
     end
 end
 fprintf('\nNumber of Products Sold Above Average Price: %d\n', aboveAvgCount);
-fprintf('\nTotal Income from Above Average Price Sales: %f\n\n', TotalTransactionAboveAvg);
+fprintf('\nTotal Income from Above Average Price Sales ($): %f\n\n', TotalTransactionAboveAvg);
 
 
 % Finding the total income from each product (use a for loop)
@@ -72,7 +72,7 @@ for i = 1:length(products)
     totalIncomeAll = totalIncomeAll + income;
 end
 
-fprintf('\nTotal Income from All Sales: %f\n', totalIncomeAll);
+fprintf('\nTotal Income from All Sales ($): %f\n', totalIncomeAll);
 
 % Debug step to check if total income from All Sales is correct
-% disp(sum(price .* quantity_2017))
+%disp(sum(price .* quantity_2017))
