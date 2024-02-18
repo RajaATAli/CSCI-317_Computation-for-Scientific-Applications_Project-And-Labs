016a4d5baf4015bb7b7a625447dbe11c34b54f2e
RajaATAli <allmdararslan13@gmail.com>
2024-02-18 14:01:39 -0500

    Removed classifyObjects.m and detectObjects.m as the logic was already integrated in preprocessVideo.m

--- Project1_MATLAB/src/classifyObjects.m
+++
@@ -1,6 +0,0 @@
-function label = classifyObjects(processedImage, model)
-    % Assuming 'model' is already loaded and 'processedImage' is ready
-    label = classify(model, processedImage);
-    disp(['Predicted Label: ', char(label)]);
-end
-
--- Project1_MATLAB/src/detectObjects.m
+++
@@ -1,5 +0,0 @@
-function [bboxes, scores, labels] = detectObjects(frame, detector)
-    [bboxes, scores, labels] = detect(detector, frame);
-    % Here, 'detector' is a pre-trained object detector model,
-    % Will be define elsewhere in project
-end
